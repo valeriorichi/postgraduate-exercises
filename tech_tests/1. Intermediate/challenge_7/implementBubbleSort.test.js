c30c7bf491bdc9ceae3e07ab0b27b3652458f077
Edd Kleszcz <edd@edds-mbp.home>
2021-11-24 21:33:05 +0000

    updating test description

--- tech_tests/1. Intermediate/challenge_7/implementBubbleSort.test.js
+++ tech_tests/1. Intermediate/challenge_7/implementBubbleSort.test.js
@@ -13,7 +13,7 @@ describe("Creating Bubble Sort", () => {
         expect(implementBubbleSort([1, 4, 2, 8, 345, 123, 43, 32, 5643, 63, 123, 43, 2, 55, 1, 234, 92])).toEqual([1, 1, 2, 2, 4, 8, 32, 43, 43, 55, 63, 92, 123, 123, 234, 345, 5643])
     })
 
-    test("Returns a new array of sorted numbers", () => {
+    test("Returns a new array", () => {
         const original = [4, 1, 5, 7, 3];
         const sorted = implementBubbleSort(original);
         expect(Array.isArray(sorted)).toBe(true);
