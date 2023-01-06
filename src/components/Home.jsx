04ad862d92318fb0afd60ce7291ecaf1c061ff95
Callum Underwood <callumunderwood@Callums-MacBook-Pro.local>
2023-01-06 10:03:46 +0000

    Fix grammar mistakes in Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,37 @@
 const Home = () => {
-    return (
-        <section>
-            <h2>Welcome to the Front End Tech Tests!</h2>
-            <p>This is the repo where you can hone your skills and sharpen your teeth on some problems you might face in an interview</p>
-            <p>We have given you 6 challenges that you can try. We suggest you build a new React App to try building these problems.</p>
-            <p>Each test demonstrates how the solution should function, how you build it is up to you, but it must have the core functionality that has been asked for!</p>
-            <p>In order, the tests are:</p>
-            <ol>
-                <li>Build a Log In Page,</li>
-                <li>Build a Movie Search Bar,</li>
-                <li>Create a "Name the 50 US States" Game,</li>
-                <li>Create a "Find the X" Game,</li>
-                <li>Build a Drop Down Menu,</li>
-                <li>Build an Pseudo Instant Messaging Service</li>
-            </ol>
-            <p>If you feel comfortable enough, try implementing the challenge section of each test!</p>
-            <p>They're harder than they appear...</p>
-            <p>Have fun and best of luck in your interviews!</p>
-        </section>
-    )
-}
+  return (
+    <section>
+      <h2>Welcome to the Front-End Tech Tests!</h2>
+      <p>
+        This is the repo where you can hone your skills and sharpen your teeth
+        on some problems you might face in an interview
+      </p>
+      <p>
+        We have given you 6 challenges that you can try. We suggest you build a
+        new React App to try addressing these problems.
+      </p>
+      <p>
+        Each test demonstrates how the solution should function, how you build
+        it is up to you, but it must have the core functionality that has been
+        asked for!
+      </p>
+      <p>In order, the tests are:</p>
+      <ol>
+        <li>Build a Log-in Page,</li>
+        <li>Build a Movie Search Bar,</li>
+        <li>Create a "Name the 50 US States" Game,</li>
+        <li>Create a "Find the X" Game,</li>
+        <li>Build a Drop-Down Menu,</li>
+        <li>Build a Pseudo Instant Messaging Service</li>
+      </ol>
+      <p>
+        If you feel comfortable enough, try implementing the challenge section
+        of each test!
+      </p>
+      <p>They're harder than they appear...</p>
+      <p>Have fun, and best of luck in your interviews!</p>
+    </section>
+  );
+};
 
 export default Home;
