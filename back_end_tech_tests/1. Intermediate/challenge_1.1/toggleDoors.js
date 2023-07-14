69034a0f5806384f3ce2aae8606d73bdbfeb62b7
valeriorichi <valeriorichi@gmail.com>
2023-07-14 23:13:28 +0100

    toggleDoors in process

--- back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
@@ -1,3 +1,15 @@
-const toggleDoors = () => {}
+const toggleDoors = (n) => {
+    const doors = Array(n).fill(1);
 
+    for (y = 2; y <= n; y++) {
+        console.log("y=", y);
+        for (i = y - 1; i < n; i = i + y) {
+
+            doors[i] = 0 ? doors[i] = 1 : doors[i] = 0;
+            console.log("i=", i, doors);
+        }
+    }
+    console.log(doors);
+};
+toggleDoors(10);
 module.exports = toggleDoors;
