9886ad70e931d82d6a1452837417412b1b7af4ea
Valeriy Yuriy <valeriorichi@gmail.com>
2023-07-15 19:09:15 +0100

    Update toggleDoors.js

--- back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.1/toggleDoors.js
@@ -1,18 +1,3 @@
 const toggleDoors = (n) => {
-    const doors = Array(n).fill(1);
-    const indexArray = [];
-    for (y = 2; y <= n; y++) {
-        for (i = y - 1; i < n; i = i + y) {
-            doors[i] === 0 ? doors[i] = 1 : doors[i] = 0;
-        }
-    }
-    for (i = 0; i < n; i++) {
-        if (doors[i] === 1) {
-            indexArray.push(i + 1);
-        }
-    }
-    console.log(indexArray);
-    return indexArray;
-};
-toggleDoors(10);
+   
 module.exports = toggleDoors;
