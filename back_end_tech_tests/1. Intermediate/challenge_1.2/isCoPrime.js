164988690da4e5827aba8653f7d03de78047950d
valeriorichi <valeriorichi@gmail.com>
2023-07-17 22:52:20 +0100

    implementing the Euclidean algorithm

--- back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.2/isCoPrime.js
@@ -11,3 +11,19 @@ const isCoPrime = (num1, num2) => {
 };
 
 module.exports = isCoPrime;
+
+/*
+implementing the Euclidean algorithm
+const gcd = (num1, num2) => {
+    while (num2 !== 0) {
+      let temp = num2;
+      num2 = num1 % num2;
+      num1 = temp;
+    }
+    return num1;
+  };
+  
+  const isCoPrime = (num1, num2) => {
+    return gcd(num1, num2) === 1;
+  };
+  */
