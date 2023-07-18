5a73f014e6512d91e4ca6b47f16a0113f124e0de
valeriorichi <valeriorichi@gmail.com>
2023-07-18 23:53:11 +0100

    some changes to inentory-update.js

--- back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
@@ -25,25 +25,8 @@ function updateInventory(curInv, newInv) {
         return 0;
     });
 
-    console.log(updatedInv);
+    return updatedInv;
 }
 
+module.exports = updateInventory;
 
-// module.exports = updateInventory;
-
-
-var curInv = [
-    [21, "Bowling Ball"],
-    [2, "Dirty Sock"],
-    [1, "Hair Pin"],
-    [5, "Microphone"]
-];
-
-var newInv = [
-    [2, "Hair Pin"],
-    [3, "Half-Eaten Apple"],
-    [67, "Bowling Ball"],
-    [7, "Toothpaste"]
-];
-
-updateInventory(curInv, newInv);
