20c3ae7c612e916bcd3700d597af0f46f4e7ea10
valeriorichi <valeriorichi@gmail.com>
2023-07-18 21:43:43 +0100

    inventoru-update.js started

--- back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
+++ back_end_tech_tests/1. Intermediate/challenge_1.4/inventory-update.js
@@ -1,3 +1,33 @@
-function updateInventory() {}
+function updateInventory(curInv, newInv) {
+    let updatedInv = [];
+    newInv.forEach(newElement => {
+        updatedInv = curInv.forEach(curElement => {
+            if (curElement[1] === newElement[1]) {
+                curElement[0] += newElement[0];
+            } else {
+                console.log(newElement);
+            }
+        });
+    });
+    console.log(curInv);
+}
 
-module.exports = updateInventory;
+
+// module.exports = updateInventory;
+
+
+var curInv = [
+    [21, "Bowling Ball"],
+    [2, "Dirty Sock"],
+    [1, "Hair Pin"],
+    [5, "Microphone"]
+];
+
+var newInv = [
+    [2, "Hair Pin"],
+    [3, "Half-Eaten Apple"],
+    [67, "Bowling Ball"],
+    [7, "Toothpaste"]
+];
+
+updateInventory(curInv, newInv);
