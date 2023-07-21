b6e4b6e35e8dffe8f0d8aadbfc8f0c1a8f42dc45
valeriorichi <valeriorichi@gmail.com>
2023-07-21 22:50:04 +0100

    reverseCapitalisation done

--- back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
@@ -1,3 +1,13 @@
-const reverseCapitalisation = () => {}
-
+const reverseCapitalisation = (inputString) => {
+    let reversedString = "";
+    for (let i = 0; i < inputString.length; i++) {
+        const char = inputString[i];
+        if (char === char.toUpperCase()) {
+            reversedString += char.toLowerCase();
+        } else {
+            reversedString += char.toUpperCase();
+        }
+    }
+    return reversedString;
+};
 module.exports = reverseCapitalisation;
