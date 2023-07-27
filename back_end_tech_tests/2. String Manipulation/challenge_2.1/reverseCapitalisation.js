43c2106428553fe41ffa88a5c7792764a26a3db2
valeriorichi <valeriorichi@gmail.com>
2023-07-27 23:57:01 +0100

    regex aproach to solve reverseCapitalisation

--- back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.1/reverseCapitalisation.js
@@ -1,14 +1,14 @@
 const reverseCapitalisation = (inputString) => {
 
-    inputString = inputString.replace(/\b[A-Z]/g, letter => letter.toLowerCase()) + inputString.replace(/\b[a-z]/g, letter => letter.toUpperCase());
-    let reverseCapitalisedString = "";
-    for (i = 0; i < inputString.length; i++) {
-        reverseCapitalisedString += inputString.charAt(i) === inputString.charAt(i).toUpperCase() ? inputString.charAt(i).toLowerCase() : inputString.charAt(i).toUpperCase();
-    }
-    return reverseCapitalisedString;
+    inputString = inputString.match(/[A-Z]/g) ? inputString.replace(/[A-Z]/g, letter => letter.toLowerCase()) : inputString.replace(/\b[a-z]/g, letter => letter.toUpperCase());
+
+    return inputString;
 };
 
-inputString = inputString.replace(pattern, letter => letter.toLowerCase());
+
+
+
+
 
 // this is for loop approach
 const reverseCapitalisation1 = (inputString) => {
