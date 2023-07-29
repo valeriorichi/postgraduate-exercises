67f8b7a71aca9a8e8a793c2c10aafe5fb5d2c8b9
valeriorichi <valeriorichi@gmail.com>
2023-07-29 23:56:31 +0100

    wordMatcher

--- back_end_tech_tests/2. String Manipulation/challenge_2.2/decryptMessage.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.2/decryptMessage.js
@@ -1,41 +1,16 @@
-//const decryptMessage = (message, shift) => {}
+const decryptMessage = (message, shift) => {
+    message = message.replace(/[a-zA-Z]/g, decryptFunc);
 
-//module.exports = decryptMessage;
-
-function decryptMessage(jumbledString, shift) {
-    const alphabet = 'abcdefghijklmnopqrstuvwxyz';
-    let decryptedMessage = '';
-
-    for (let i = 0; i < jumbledString.length; i++) {
-        const char = jumbledString[i];
-        let index = alphabet.indexOf(char.toLowerCase());
-
-        if (index !== -1) {
-            // Applying the shift in the opposite direction
-            let shiftedIndex = (index - shift) % 26;
-
-            // Handling negative indexes (looping back to the end of the alphabet)
-            if (shiftedIndex < 0) {
-                shiftedIndex = 26 + shiftedIndex;
-            }
-
-            // Keeping the original case (uppercase or lowercase)
-            if (char === char.toUpperCase()) {
-                decryptedMessage += alphabet[shiftedIndex].toUpperCase();
-            } else {
-                decryptedMessage += alphabet[shiftedIndex];
-            }
-        } else {
-            // For non-alphabetic characters, simply add them to the decrypted message as is
-            decryptedMessage += char;
+    decryptFunc = (letter) => {
+        let newCode = letter.toUpperCase.charCodeAt(0) + shift;
+        if (newCode >= 65 || newCode <= 90) {
+            return newLetter = letter === letter.toUpperCase ? newCode : newCode + 32;
         }
-    }
+    };
 
-    return decryptedMessage;
-}
+    let text = String.fromCharCode(72, 69, 76, 76, 79);
+};
 
-// Example usage:
-const jumbledString = "def";
-const shift = 2;
-const decryptedMessage = decryptMessage(jumbledString, shift);
-console.log(decryptedMessage); // Output: "bcd"//
+module.exports = decryptMessage;
+
+return inputString = inputString.replace(/[a-zA-Z]/g, letter => letter === letter.toUpperCase() ? letter.toLowerCase() : letter.toUpperCase());
--- back_end_tech_tests/2. String Manipulation/challenge_2.4/wordMatcher.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.4/wordMatcher.js
@@ -1,3 +1,13 @@
-const wordMatcher = (matcher, str) => {}
+const wordMatcher = (matcher, str) => {
+    // Using a regular expression with word boundaries (\b) to match whole words
+    const regex = new RegExp(`\\b${matcher}\\b`, 'gi');
+    const matches = str.match(regex);
+
+    if (matches) {
+        return matches.length;
+    } else {
+        return 0;
+    }
+};
 
 module.exports = wordMatcher;
