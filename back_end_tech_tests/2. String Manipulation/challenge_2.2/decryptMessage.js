24462618d06145db130dab391e3edcbb3c60f156
valeriorichi <valeriorichi@gmail.com>
2023-08-02 00:09:58 +0100

    working on decryptMessage

--- back_end_tech_tests/2. String Manipulation/challenge_2.2/decryptMessage.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.2/decryptMessage.js
@@ -1,16 +1,14 @@
 const decryptMessage = (message, shift) => {
 
-    const charArray = [...message];
+    const charArray = [...message.toLowerCase()];
     const decyptedCharCodeArray = [];
 
     charArray.forEach((char) => {
-        let newCode = char.toLowerCase().charCodeAt(0) - (shift - Math.floor(shift / 28) * 28);
+        let newCode = char.charCodeAt(0) - (shift % 26);
         switch (true) {
-            case (char.toLowerCase().charCodeAt(0) < 97 || char.toLowerCase().charCodeAt(0) > 122):
+            case (char.charCodeAt(0) < 97 || char.charCodeAt(0) > 122):
                 decyptedCharCodeArray.push(char.charCodeAt(0));
                 break;
-
-
             case newCode < 97:
                 decyptedCharCodeArray.push(newCode + 26);
                 break;
@@ -22,8 +20,7 @@ const decryptMessage = (message, shift) => {
                 break;
         }
     });
-    console.log(message, shift,
-        "Arraymy----->>>", decyptedCharCodeArray, String.fromCharCode(...decyptedCharCodeArray));
+
     return String.fromCharCode(...decyptedCharCodeArray);
 
 };
