04077458d0ca80b82975e9ece87a2d233087522e
valeriorichi <valeriorichi@gmail.com>
2023-08-03 22:51:57 +0100

    decodeBinary

--- back_end_tech_tests/2. String Manipulation/challenge_2.5/decodeBinary.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.5/decodeBinary.js
@@ -2,9 +2,13 @@ function decodeBinary(binaryString) {
     // Split the binary string into an array of 8-bit chunks
     const binaryArray = binaryString.match(/.{1,8}/g);
 
+    // Convert each binary chunk to its decimal representation
+    const decimalArray = binaryArray.map((binaryChunk) => parseInt(binaryChunk, 2));
+
+    // Convert the decimal array to characters
+    const text = decimalArray.map((decimal) => String.fromCharCode(decimal)).join('');
 
 
-    return text;
 }
 
 module.exports = decodeBinary;
