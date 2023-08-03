77fe237bee49e2c96007b5d98429379c9324a182
valeriorichi <valeriorichi@gmail.com>
2023-08-03 22:51:23 +0100

    decodeBinary started

--- back_end_tech_tests/2. String Manipulation/challenge_2.5/decodeBinary.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.5/decodeBinary.js
@@ -1,3 +1,19 @@
+function decodeBinary(binaryString) {
+    // Split the binary string into an array of 8-bit chunks
+    const binaryArray = binaryString.match(/.{1,8}/g);
+
+
+
+    return text;
+}
+
+module.exports = decodeBinary;
+
+
+
+/*
+
 function decodeBinary(binaryString) {}
 
 module.exports = decodeBinary;
+*/
