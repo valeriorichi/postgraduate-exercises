9f8e660af007e173cb7972ef2dbb77fae11c0d79
valeriorichi <valeriorichi@gmail.com>
2023-08-04 23:32:53 +0100

    isWebAddress alternative solution

--- back_end_tech_tests/2. String Manipulation/challenge_2.3/isWebAddress.js
+++ back_end_tech_tests/2. String Manipulation/challenge_2.3/isWebAddress.js
@@ -2,3 +2,10 @@ module.exports = /^(http(s){0,1}:\/\/){0,1}(www\.){0,1}\w+\.\w{2,3}/; // /REGEX_
 
 const webAddressRegex = /^(https?:\/\/)?([\da-z.-]+)\.([a-z.]{2,6})([/\w .-]*)*\/?$/i;
 
+const isWebAddress = (str) => webAddressRegex.test(str);
+
+
+const address1 = "https://www.example.com";
+const address2 = "http://subdomain.example.co.uk/page?param=value";
+console.log(isWebAddress(address1));
+console.log(isWebAddress(address2));
