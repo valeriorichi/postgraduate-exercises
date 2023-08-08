c869da2f30ec8f5fc27508d9ca54535bd6e87548
Valeriy Yuriy <valeriorichi@gmail.com>
2023-08-08 22:49:56 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -16,3 +16,25 @@ function sym(...arrays) {
                                 }
   
 sym([1, 2, 3], [5, 2, 1, 4]);
+
+
+
+function sym(...args) {
+  // Return the symmetric difference of 2 arrays
+  const getDiff = (arr1, arr2) => {
+    // Returns items in arr1 that don't exist in arr2
+    function filterFunction(arr1, arr2) {
+      return arr1.filter(item => arr2.indexOf(item) === -1);
+    }
+
+    // Run filter function on each array against the other
+    return filterFunction(arr1, arr2).concat(filterFunction(arr2, arr1));
+  };
+
+  // Reduce all arguments getting the difference of them
+  const summary = args.reduce(getDiff, []);
+
+  // Run filter function to get the unique values
+  const unique = summary.filter((elem, index, arr) => index === arr.indexOf(elem));
+  return unique;
+                              }
