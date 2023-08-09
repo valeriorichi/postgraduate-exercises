bac4694f8e3986cb2738006bd80c8f2958f7569e
Valeriy Yuriy <valeriorichi@gmail.com>
2023-08-09 19:09:34 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -38,3 +38,17 @@ function sym(...args) {
   const unique = summary.filter((elem, index, arr) => index === arr.indexOf(elem));
   return unique;
                               }
+
+
+
+
+
+const diff = (arr1, arr2) => [
+  ...arr1.filter(e => !arr2.includes(e)),
+  ...arr2.filter(e => !arr1.includes(e))
+];
+
+const sym = (...args) => [...new Set(args.reduce(diff))];
+
+// test here
+sym([1, 2, 3], [5, 2, 1, 4]);
