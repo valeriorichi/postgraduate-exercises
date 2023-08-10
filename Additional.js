25a968de043abc75776272a85a38a6975fb17b3a
Valeriy Yuriy <valeriorichi@gmail.com>
2023-08-10 22:28:19 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -52,3 +52,8 @@ const sym = (...args) => [...new Set(args.reduce(diff))];
 
 // test here
 sym([1, 2, 3], [5, 2, 1, 4]);
+
+
+
+Inventory Update
+Compare and update the inventory stored in a 2D array against a second 2D array of a fresh delivery. Update the current existing inventory item quantities (in arr1). If an item cannot be found, add the new item and quantity into the inventory array. The returned inventory array should be in alphabetical order by item.
