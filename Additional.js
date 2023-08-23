d2451ed7538308d734ebce58fda422ff0c04e0a6
Valeriy Yuriy <valeriorichi@gmail.com>
2023-08-23 23:08:21 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -336,3 +336,21 @@ function mergeSort(array) {
   //Return the merged sorted array
   return merger(left, right);
 }
+
+
+var str = ???;
+permAlone(current position in original string, characters used already in original string, created string) {
+  if (current string is finished) {
+    print current string;
+  } else {
+    for (var i = 0; i < str.length; i++) {
+      if (str[i] has not been used) {
+        put str[i] into the current position of new string;
+        mark str[i] as used;
+        permAlone(current position in original string, characters used already in original string, created string);
+        remove str[i] as used because another branch in the tree for i + 1 will likely use it;
+      }
+    }
+  }
+}
+permAlone(0, nothing used yet, empty new string (or array the same size as str));k
