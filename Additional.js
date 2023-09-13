81a6fe34e976ae83a6050556cdd8ac76900fc4d4
Valeriy Yuriy <valeriorichi@gmail.com>
2023-09-13 23:34:52 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -525,3 +525,26 @@ var newInv = [
 ];
 
 updateInventory(curInv, newInv);
+
+
+function quickSort(array) {
+  if (array.length === 0) {
+    return [];
+  } else {
+    const pivotValue = array[0];
+    // Sort elements into three piles
+    let lesser = [];
+    let equal = [];
+    let greater = [];
+    for (let e of array) {
+      if (e < pivotValue) {
+        lesser.push(e);
+      } else if (e > pivotValue) {
+        greater.push(e);
+      } else {
+        equal.push(e);
+      }
+    }
+    return [...quickSort(lesser), ...equal, ...quickSort(greater)];
+  }
+}
