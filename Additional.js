d42df38c266f9c4d88227e42f35b2b6f14c7ff31
Valeriy Yuriy <valeriorichi@gmail.com>
2023-10-09 22:49:44 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -548,3 +548,21 @@ function quickSort(array) {
     return [...quickSort(lesser), ...equal, ...quickSort(greater)];
   }
 }
+
+
+const toggleDoors = (n) => {
+    const doors = Array(n).fill(1);
+    const indexArray = [];
+    for (y = 2; y <= n; y++) {
+        for (i = y - 1; i < n; i = i + y) {
+            doors[i] === 0 ? doors[i] = 1 : doors[i] = 0;
+        }
+    }
+    for (i = 0; i < n; i++) {
+        if (doors[i] === 1) {
+            indexArray.push(i + 1);
+        }
+    }
+    return indexArray;
+};
+module.exports = toggleDoors;
