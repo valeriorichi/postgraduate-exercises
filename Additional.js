1239eee7f9808170bc4930fe6343353eb2d90aa4
Valeriy Yuriy <valeriorichi@gmail.com>
2023-10-11 23:48:18 +0100

    Update Additional.js

--- Additional.js
+++ Additional.js
@@ -613,3 +613,16 @@ function permAlone(str) {
 
 // Test here.
 permAlone("aab");
+
+
+
+function insertionSort(array) {
+  for (let i = 1; i < array.length; i++) {
+    let curr = array[i];
+    for (var j = i - 1; j >= 0 && array[j] > curr; j--) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = curr;
+  }
+  return array;
+}
